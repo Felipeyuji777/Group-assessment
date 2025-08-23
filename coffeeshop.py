1e79876816acead079d194f528843786f4f94e0f
Felipe Yuji <felipehizumi777@gmail.com>
2025-08-23 12:38:49 +0700

    im abt to crash out

--- coffeeshop.py
+++ coffeeshop.py
@@ -10,27 +10,36 @@ menu = [
 ]
 
 print("Welcome to BUV Beans and Leaves!")
+print("1. View Menu")
+print("2. Place Order")
+print("3. Cancel Order")
+print("4. View Current Orders")
+print("5. Calculate Order Total")
+print("6. Complete Order")
+print("7. Show Total Income")
 while True:
-    print("1. View Menu")
-    print("2. Place Order")
-    print("3. Cancel Order")
-    print("4. View Current Orders")
-    print("5. Calculate Order Total")
-    print("6. Complete Order")
-    print("7. Show Total Income")
+        option = int(input("Select an option (1/2/3/4/5/6/7): "))
 
-    option = int(input("Select an option (1/2/3/4/5/6/7): "))
-
-    if option == 1:
-        print("==== MENU ====")
-        for item in menu:
-            print(item)
-    elif option == 2:
-        items = input("Select your order: ")
-        orders.append(items)
-    elif option == 3:
-        cancel = input("Cancel an order: ")
-        orders.remove(cancel)
-    elif option == 4:
-        print(f"Your current orders:{orders}")
-
+        if option == 1:
+            print("==== MENU ====")
+            for item in menu:
+                print(item)
+        elif option == 2:
+            items = input("Select your order: ")
+            orders.append(items)
+        elif option == 3:
+            cancel = input("Cancel an order: ")
+            if cancel in orders:
+                orders.remove(cancel)
+            else:
+                print(f"{cancel} not found")
+        elif option == 4:
+            print(f"Your current orders:{orders}")
+        elif option == 5:
+            total = 0
+            for order in orders:
+                for item in menu:
+                    if order.lower() == item["name"].lower():  # case-insensitive match
+                        total += item["price"]
+                    break
+            print(f"Your total order amount is: {total} VND")
